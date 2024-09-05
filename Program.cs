37da6e32383cc9db0617764c139c06398a1f6e8e
amaliebra <jba082024@gmail.com>
2024-09-05 10:59:46 +0200

    edit1

--- Program.cs
+++ Program.cs
@@ -1,11 +1,19 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => { return 10; });
 
-app.MapGet("/fire", () =>
-{
-    return "Yooo! this is lit!";
-});
+//GET ALL Movies
+app.MapGet("/movies", () => "returns all movies");
+
+// create add a movie
+app.MapPost("/movies", () => "add a new movie");
+
+//delete a movie 
+app.MapDelete("/movies/{id}", (int id) => $"delete movie with id: {id}");
+
+// Update a movies
+app.MapPut("/movies/{id}", (int id) => $"update movie with id: {id}");
+
+app.MapGet("/fire", () => "Yooo! this is lit!");
 
 app.Run();
